4af20b022e6669ae08ec866be6ffdb7b2b3204f3
jirigalis <jiri@infinetcloud.com>
2026-02-17 09:52:28 +0100

    Fix Vercel Analytics injection

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { inject as vercelInject } from '@vercel/analytics'
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
@@ -8,6 +9,9 @@ export const appConfig: ApplicationConfig = {
   providers: [
       provideZoneChangeDetection({ eventCoalescing: true }),
       provideRouter(routes),
-      provideHttpClient()
+      provideHttpClient(),
+      provideAppInitializer(() => {
+          vercelInject({ mode: isDevMode() ? 'development' : 'production' });
+      })
   ]
 };
