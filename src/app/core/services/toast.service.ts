38123dfd415c775342ff49f79ac18011ac5e7fb3
jirigalis <jiri@infinetcloud.com>
2026-02-18 21:54:04 +0100

    Fix toast glitch

--- src/app/core/services/toast.service.ts
+++ src/app/core/services/toast.service.ts
@@ -16,6 +16,7 @@ export class ToastService {
         this.toasts.update(current => [...current, newToast]);
 
         setTimeout(() => {
+            console.log('Remove toast', id);
             this.remove(id);
         }, duration || this.DEFAULT_DURATION);
     }
