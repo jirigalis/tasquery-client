04e828d279b75122f425a7b89e2e2de6b091dedc
jirigalis <jiri@infinetcloud.com>
2026-02-20 17:26:04 +0100

    Fix production api url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,9 @@
-import { ApplicationConfig, isDevMode, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, provideAppInitializer, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { inject as vercelInject } from '@vercel/analytics'
 
 import { routes } from './app.routes';
 import { provideHttpClient } from '@angular/common/http';
-import { environment } from '../environments/environment';
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -13,10 +12,6 @@ export const appConfig: ApplicationConfig = {
       provideHttpClient(),
       provideAppInitializer(() => {
           vercelInject();
-
-          // 2. Pro tvůj osobní debug raději používej tvůj environment soubor
-          console.log('Production mode:', environment.production);
-          console.log('isDevMode()', isDevMode());
       }),
   ]
 };
